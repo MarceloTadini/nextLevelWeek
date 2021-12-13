566bf86b100ea4d4f7d93fb047bfcffb1fef9600
Marcelo Tadini <tadini.marcelo53@gmail.com>
2021-12-13 19:18:30 -0300

    Ajuste

--- src/database/db.js
+++ src/database/db.js
@@ -10,51 +10,51 @@ module.exports = db
 //Utilizar o objeto de banco de dados para operações
 db.serialize(() => {
     //1 criar uma tabela com comandos sql
-    /*  db.run(`
-        CREATE TABLE IF NOT EXISTS places (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            image TEXT,
-            name TEXT,
-            address TEXT,
-            address2 TEXT,
-            state TEXT,
-            city TEXT,
-            items TEXT
-        );
-    `)
+       db.run(`
+         CREATE TABLE IF NOT EXISTS places (
+             id INTEGER PRIMARY KEY AUTOINCREMENT,
+             image TEXT,
+             name TEXT,
+             address TEXT,
+             address2 TEXT,
+             state TEXT,
+             city TEXT,
+             items TEXT
+         );
+     `)
 
-    //2 Inserir dados
-    const query = `
-        INSERT INTO places (
-            image,
-            name,
-            address,
-            address2,
-            state,
-            city,
-            items
-        ) VALUES (?,?,?,?,?,?,?);
+     //2 Inserir dados
+     const query = `
+         INSERT INTO places (
+             image,
+             name,
+             address,
+             address2,
+             state,
+             city,
+             items
+         ) VALUES (?,?,?,?,?,?,?);
 `   
-    const values = [
-        "https://https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80images.unsplash.com/photo-1518792528501-352f829886dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        "Papersider",
-        "Guilhermer Gembala, Jardim América",
-        "Número 260",
-        "Santa Catarina",
-        "Rio do sul",
-        "Resíduos eletrônicos e lâmpadas"
-    ]
+     const values = [
+         "Link da imagem",
+         "Testando",
+         "Rua do teste, Testando",
+         "Número 260",
+         "Minas Gerais",
+         "Itajubá",
+         "Resíduos eletrônicos e lâmpadas"
+     ]
 
-    function afterInsertData(err) {
-        if(err) {
-            return console.log(err)
-        }
+     function afterInsertData(err){
+         if(err) {
+             return console.log(err)
+         }
 
-        console.log("Cadastrado com sucesso")
-        console.log(this)
-    }
+         console.log("Cadastrado com sucesso")
+         console.log(this)
+     }
 
-    db.run(query, values, afterInsertData) 
+    //db.run(query, values, afterInsertData) 
  
     //3 Consultar os dados da tabela
      db.all(`SELECT * FROM places`, function(err, rows){
@@ -66,13 +66,13 @@ db.serialize(() => {
         console.log(rows)
     }) 
 
- */
+ 
     //4 Deletar um dado da tabela
-  /*    db.run(`DELETE FROM places WHERE id = ?`, [1], function(err){
-        if(err) {
-            return console.log(err)
-        }
+        db.run(`DELETE FROM places WHERE id = ?`, [2], function(err){ //`DELETE FROM places WHERE id = ?`, [1], 
+          if(err) {
+              return console.log(err)
+          }
         
-        console.log("Registro deletado com sucesso")
-    })  */
+          console.log("Registro deletado com sucesso");
+         }) 
 })
